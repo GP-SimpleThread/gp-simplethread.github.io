adc5f4ee81a2ae86d10c40c3f2a8e3aa6a0414a7
Genbu Hase <hasegenbu@gmail.com>
2018-05-05 00:27:52 +0900

    Reset libraries

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-const cookieStore = new CookieStore();
-cookieStore.set("GP-ST-rootUrl", location.href.replace(location.search, "").replace(/\/#?$/, ""));
-
-const ROOTURL = cookieStore.get("GP-ST-rootUrl");
-
 window.addEventListener("DOMContentLoaded", () => {
-	RootLinker.apply(ROOTURL);
+
 });
